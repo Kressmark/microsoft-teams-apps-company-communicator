a800f38da31911de8536ab4f105be5e216a655a3
Mattias Kressmark <47445738+Kressmark@users.noreply.github.com>
2021-06-11 14:21:03 +0200

    Update SendFunction.cs

--- Source/CompanyCommunicator.Send.Func/SendFunction.cs
+++ Source/CompanyCommunicator.Send.Func/SendFunction.cs
@@ -137,6 +137,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Send.Func
 
                 // Send message.
                 var messageActivity = await this.GetMessageActivity(messageContent);
+                messageActivity.Summary = "From Carlsberg";
                 var response = await this.messageService.SendMessageAsync(
                     message: messageActivity,
                     serviceUrl: messageContent.GetServiceUrl(),
